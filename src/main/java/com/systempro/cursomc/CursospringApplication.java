81fcc1df1eb19e55a013f2b4a0096311e90e4c1d
fernandolilo <69657101+fernandolilo@users.noreply.github.com>
2021-02-14 19:24:26 -0300

    Criando operacao de instaciacao

--- src/main/java/com/systempro/cursomc/CursospringApplication.java
+++ src/main/java/com/systempro/cursomc/CursospringApplication.java
@@ -1,13 +1,33 @@
 package com.systempro.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringApplication {
+import com.systempro.cursomc.domain.Categoria;
+import com.systempro.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriarepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }

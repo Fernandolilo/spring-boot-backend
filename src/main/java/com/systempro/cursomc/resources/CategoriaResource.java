8885cae9e1a6e7c5172954c65d5835aa30cff167
fernandolilo <69657101+fernandolilo@users.noreply.github.com>
2021-02-19 19:34:53 -0300

    Listando todas as categorias comDTO

--- src/main/java/com/systempro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/systempro/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,10 @@
 package com.systempro.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.systempro.cursomc.domain.Categoria;
+import com.systempro.cursomc.dto.CategoriaDTO;
 import com.systempro.cursomc.services.CategoriaService;
 
 
@@ -20,7 +26,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	
-	// metodo de busca por dados
+	// metodo de busca por dados do ID categoria
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {		
 		Categoria obj = service.find(id);				
@@ -51,6 +57,14 @@ public class CategoriaResource {
 		public ResponseEntity<Void> delete(@PathVariable Integer id) {		
 			service.delete(id);
 			return ResponseEntity.noContent().build();
+		}
+		
+		// metodo de busca por todas categorias
+		@RequestMapping(method=RequestMethod.GET)
+		public ResponseEntity<List <CategoriaDTO>> findAll() {		
+			List<Categoria> obj = service.findAll();
+			List<CategoriaDTO> listadto = obj.stream().map(dados -> new CategoriaDTO(dados)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listadto);		
 		}
 	
 }
